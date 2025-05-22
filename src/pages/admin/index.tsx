d82750338dc41c0cbb9a19585c7181e2eb5c5b5e
asep3003 <saepulloha133@gmail.com>
2025-05-23 01:23:27 +0700

    feat: add menu

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,5 +1,15 @@
 import { Button } from "@/components/ui/button";
 import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuGroup,
@@ -8,6 +18,17 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import {
   Table,
   TableBody,
@@ -16,14 +37,17 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Textarea } from "@/components/ui/textarea";
 import supabase from "@/lib/db";
 import { IMenu } from "@/types/menu";
 import { Ellipsis } from "lucide-react";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
+import { toast } from "sonner";
 
 const AdminPage = () => {
   const [menus, setMenus] = useState<IMenu[]>([]);
+  const [createDialog, setCreateDialog] = useState(false);
 
   useEffect(() => {
     const fetchMenus = async () => {
@@ -36,11 +60,115 @@ const AdminPage = () => {
     fetchMenus();
   }, [supabase]);
 
+  const handleAddMenu = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+
+    try {
+      const { data, error } = await supabase
+        .from("menus")
+        .insert(Object.fromEntries(formData))
+        .select();
+
+      if (error) console.log("error: ", error);
+      else {
+        if (data) {
+          setMenus((prev) => [...prev, ...data]);
+        }
+        toast("Menu added successfully");
+        setCreateDialog(false);
+      }
+    } catch (error) {
+      console.log("error: ", error);
+    }
+  };
+
   return (
     <div className="container mx-auto py-8">
       <div className="mb-4 w-full flex justify-between">
         <div className="text-3xl font-bold">Menu</div>
-        <Button className="font-bold">Add Menu</Button>
+        <Dialog open={createDialog} onOpenChange={setCreateDialog}>
+          <DialogTrigger asChild>
+            <Button className="font-bold">Add Menu</Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-md">
+            <form onSubmit={handleAddMenu} className="space-y-4">
+              <DialogHeader>
+                <DialogTitle>Add Menu</DialogTitle>
+                <DialogDescription>
+                  Create a new menu by insert data in this form.
+                </DialogDescription>
+              </DialogHeader>
+              <div className="grid w-full gap-4">
+                <div className="grid w-ull gap-1.5">
+                  <Label htmlFor="name">Name</Label>
+                  <Input
+                    id="name"
+                    name="name"
+                    placeholder="Insert Name"
+                    required
+                  />
+                </div>
+                <div className="grid w-ull gap-1.5">
+                  <Label htmlFor="price">Price</Label>
+                  <Input
+                    id="price"
+                    name="price"
+                    placeholder="Insert Price"
+                    required
+                  />
+                </div>
+                <div className="grid w-ull gap-1.5">
+                  <Label htmlFor="image">Image</Label>
+                  <Input
+                    id="image"
+                    name="image"
+                    placeholder="Insert Image"
+                    required
+                  />
+                </div>
+                <div className="grid w-ull gap-1.5">
+                  <Label htmlFor="category">Category</Label>
+                  <Select name="category" required>
+                    <SelectTrigger className="w-full">
+                      <SelectValue placeholder="Select Category" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectLabel>Category</SelectLabel>
+                        <SelectItem value="Coffee">Coffee</SelectItem>
+                        <SelectItem value="Non Coffee">Non Coffee</SelectItem>
+                        <SelectItem value="Pastries">Pastries</SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                </div>
+                <div className="grid w-ull gap-1.5">
+                  <Label htmlFor="description">Description</Label>
+                  <Textarea
+                    id="description"
+                    name="description"
+                    placeholder="Insert Description"
+                    required
+                    className="resized-none h-32"
+                  />
+                </div>
+              </div>
+              <DialogFooter>
+                <DialogClose>
+                  <Button variant="secondary" className="cursor-pointer">
+                    Cancel
+                  </Button>
+                </DialogClose>
+                <DialogClose>
+                  <Button type="submit" className="cursor-pointer">
+                    Create
+                  </Button>
+                </DialogClose>
+              </DialogFooter>
+            </form>
+          </DialogContent>
+        </Dialog>
       </div>
       <div>
         <Table>
