3c939d5cb2fde9efca4bf827f1ec50a07dfe5d26
asep3003 <saepulloha133@gmail.com>
2025-06-05 06:11:21 +0700

    fix: next image domain

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,12 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   images: {
-    domains: ["img.freepik.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.freepik.com",
+      },
+    ],
   },
 };
 
